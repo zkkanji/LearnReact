80b03a559080e7c24f95e8b4a08d0ece21480305
zkkanji <zkkanji5@gmail.com>
2026-02-10 21:56:21 -0500

    Update App.tsx

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from"./components/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let items = ["New York", "San Francisco", "Paris", "Tokyo", "London"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  const text = "Learn React";
+  const names = ["Zain", "Kanji"]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ListGroup
+        items={items}
+        heading={"Cities"}
+        onSelectItem={handleSelectItem}
+      />
+      <br></br>
+      <Alert>
+        {text}
+      </Alert>
+      <Button names={names}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
