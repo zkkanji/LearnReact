5c8c4ce6fe0d731358b1af5911ab6a2855a242a8
zkkanji <zkkanji5@gmail.com>
2026-02-10 21:59:41 -0500

    Update App.tsx

--- reactTest/src/App.tsx
+++ reactTest/src/App.tsx
@@ -1,9 +1,41 @@
-//Import your component
-//Don't forget to import images
+import Card from "./components/Card";
+import img1 from "./assets/img1.jpg";
+import img2 from "./assets/img2.jpg";
+import img3 from "./assets/img3.jpg";
+
 const App = () => {
+  const images = [img1, img2, img3];
+
+  const locTitles = ["New York", "Caves", "Neptune"];
+  const descriptions = [
+    "The greatest city in the US",
+    "Explore the unknown",
+    "The planet of frost",
+  ];
+  const buttonText = "Open";
+  const buttonLinks = [
+    "https://en.wikipedia.org/wiki/New_York_City",
+    "https://en.wikipedia.org/wiki/Cave",
+    "https://en.wikipedia.org/wiki/Neptune",
+  ];
+
   return (
-    <div className="d-flex justify-content-around gap-4 p-4 flex-wrap">
-      Your Implementation Here
+    <div>
+      <ul
+        style={{ listStyle: "none", padding: 0, display: "flex", gap: "20px" }}
+      >
+        {locTitles.map((title, index) => (
+          <li key={index}>
+            <Card
+              title={title}
+              description={descriptions[index]}
+              img={images[index]}
+              buttonText={buttonText}
+              buttonLink={buttonLinks[index]}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
